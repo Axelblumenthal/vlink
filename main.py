8fddff0640c18085e9074a920daed4bdf83c5d68
Axelblumenthal <Axel1Blumenthal1@gmx.de>
2023-12-29 15:42:18 +0100

    time

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import display
 import button
 import led
-
+import time
 
 
 
@@ -9,4 +9,4 @@ print("VLINK Version 0.2")
 while True:
     display.mainpage()
     display.get_info(False) # print debug data on screen
-    button.get_input()
+    button.get_input() #test buttons
