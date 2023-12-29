a38e62b1aa9b6714fced354d5d1414ec68c1155f
Axelblumenthal <Axel1Blumenthal1@gmx.de>
2023-12-29 17:07:55 +0100

    print ssid

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ print(dir_list)
 
 while True:
     
-    IP, Temp, RSSI = display.get_info(False)
+    IP, Temp, RSSI ,SSID= display.get_info(False)
     #display.infopage(IP,Temp,RSSI)
     display.mainpage(str(RSSI,'utf-8')[:2])
-
+    print(SSID)
     
     #display.get_info(False) # print debug data on screen
     button.get_input() #test buttons
