be8971ff74682ed1cf76fe8ace237b463c8645c1
Axelblumenthal <Axel1Blumenthal1@gmx.de>
2023-12-29 16:46:14 +0100

    str

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ while True:
     
     IP, Temp, RSSI = display.get_info(False)
     #display.infopage(IP,Temp,RSSI)
-    display.mainpage(40)
+    display.mainpage(str(RSSI,'utf-8')[:2])
+
     
-    print(str(RSSI,'utf-8')[:2])
     #display.get_info(False) # print debug data on screen
     button.get_input() #test buttons
     time.sleep(2)
