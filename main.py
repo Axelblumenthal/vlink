ae5f0deba684f60299c23d9fc73b82ce0b50c248
Axelblumenthal <Axel1Blumenthal1@gmx.de>
2023-12-29 16:15:41 +0100

    print ifno

--- main.py
+++ main.py
@@ -12,10 +12,9 @@ print("VLINK Version 0.2")
 
 while True:
     #display.mainpage()
-    info = display.get_info
 
-    display.infopage(info[1],info[2],info[3])
-    
+    display.infopage(display.get_info)
+
     display.get_info(False) # print debug data on screen
     button.get_input() #test buttons
 
