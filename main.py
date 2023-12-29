7833f9005e202a494744527be8699093a728ccae
Axelblumenthal <Axel1Blumenthal1@gmx.de>
2023-12-29 15:48:43 +0100

    keyboard stop

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import display
 import button
 import led
 import time
+import keyboard
 
-
+def stop_program():
+    print("Program stopped")
+    exit(0)  # Exiting the program when 'Esc' or 'Ctrl + C' is pressed
+    
+# Listen for 'Esc' key or 'Ctrl + C' to stop the program
+keyboard.add_hotkey('esc', stop_program)
+keyboard.add_hotkey('ctrl+c', stop_program)
 
 print("VLINK Version 0.2")
+
+
 while True:
     display.mainpage()
     display.get_info(False) # print debug data on screen
