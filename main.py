f85bfc7982b8136bde381b19571a0d070babcec0
Axelblumenthal <Axel1Blumenthal1@gmx.de>
2023-12-29 21:19:53 +0100

    button quark

--- main.py
+++ main.py
@@ -37,9 +37,9 @@ try:
 
         if page == 1:
             display.mainpage(str(RSSI,'utf-8')[:2],str(SSID,'utf-8'))
-        if page == 0:
-            display.infopage(IP,Temp,RSSI)
         if page == 2:
+            display.infopage(IP,Temp,RSSI)
+        if page == 0:
             display.setting()
             
 
